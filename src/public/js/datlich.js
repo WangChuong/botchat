fcaea6edd0bf00b6c9a3e1c0c2d83ec010d74001
Lê Quang Trường <quangtruongle369@gmail.com>
2022-12-05 13:09:59 +0700

    upload

--- src/public/js/datlich.js
+++ src/public/js/datlich.js
@@ -9,7 +9,7 @@
 window.extAsyncInit = function () {
     // the Messenger Extensions JS SDK is done loading 
 
-    MessengerExtensions.getContext('753933095237424',
+    MessengerExtensions.getContext('5700962859998048',
         function success(thread_context) {
             // success
             //set psid to input
