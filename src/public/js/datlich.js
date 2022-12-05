ed19e4d70a91b06d0a1fca7f2d43d6be03a998c8
Lê Quang Trường <quangtruongle369@gmail.com>
2022-12-05 13:10:48 +0700

    upload

--- src/public/js/datlich.js
+++ src/public/js/datlich.js
@@ -18,7 +18,7 @@ window.extAsyncInit = function () {
         },
         function error(err) {
             // error
-            console.log('Lỗi đặt bàn Eric bot', err);
+            console.log('Lỗi đặt lịch bot', err);
         }
     );
 };
